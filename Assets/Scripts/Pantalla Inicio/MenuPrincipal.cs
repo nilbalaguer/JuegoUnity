a119fc55330b27776ec966b4241459c3a4d93ccf
AniolCarceller <aniolcarcellerfp@ibf.cat>
2025-05-13 17:57:31 +0200

    Menu Principal 2

--- Assets/Scripts/Pantalla Inicio/MenuPrincipal.cs
+++ Assets/Scripts/Pantalla Inicio/MenuPrincipal.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
+    [SerializeField] GameObject panelControlesBool;
+    void Start(){
+        panelControlesBool.SetActive(false);
+    }
     public void Salir(){
         Debug.Log("Salir...");
         Application.Quit();
-
     }
     public void Jugar(){
         SceneManager.LoadScene("Nivel");
+    }
+    public void Controles(){
+        Debug.Log("Controles");
+        panelControlesBool.SetActive(true);
+    }
+    public void salirControles(){
+        Debug.Log("Salir Controles");
+        panelControlesBool.SetActive(false);
     }
 }
