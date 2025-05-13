4c86b9564b1da0ce5271d1e92c16fbaf30399668
AniolCarceller <aniolcarcellerfp@ibf.cat>
2025-05-13 15:21:20 +0200

    Pantalla Inicio 1

---
+++ Assets/Scripts/Pantalla Inicio/MenuPrincipal.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Salir(){
+        Debug.Log("Salir...");
+        Application.Quit();
+
+    }
+    public void Jugar(){
+        SceneManager.LoadScene("Nivel");
+    }
+}
