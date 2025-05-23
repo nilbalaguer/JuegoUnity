4ca3d0c955c6b68acbd5956e143aab335442ff87
AniolCarceller <aniolcarcellerfp@ibf.cat>
2025-05-23 15:35:19 +0200

    Animaciones

--- Assets/Scripts/Pantalla Inicio/MenuPrincipal.cs
+++ Assets/Scripts/Pantalla Inicio/MenuPrincipal.cs
@@ -11,7 +11,7 @@ public class MenuPrincipal : MonoBehaviour
         Application.Quit();
     }
     public void Jugar(){
-        SceneManager.LoadScene("NIVELANIOL");
+        SceneManager.LoadScene("Piso1NivelAniol");
     }
     public void Controles(){
         Debug.Log("Controles");
